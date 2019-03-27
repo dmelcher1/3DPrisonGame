e90992c951bfc510df1190b7c112a83b66139440
Nick <ncazmay@student.fitchburgstate.edu>
2019-03-27 09:23:54 -0400

    File Shifts

--- Prisonbreak/Assets/Scripts/PlayerController.cs
+++ Prisonbreak/Assets/Scripts/PlayerController.cs
@@ -12,6 +12,8 @@ public class PlayerController : MonoBehaviour {
     public Camera playerCam;
     [SerializeField]
     private float directionSpeed = 3.0f;
+    [SerializeField]
+    private float rotDeg = 120f;
 
     private float speed = 0.0f;
     private float direction = 0.0f;
@@ -20,8 +22,8 @@ public class PlayerController : MonoBehaviour {
 
     private float playerRot = 80.0f;
     private float playerRun = 5.0f;
-
     private float controlDeadZone = 0.5f; //NEEDS ADJUSTING TO ALLOW PIVOTING WITHOUT MOVING
+    private AnimatorStateInfo stateInfo;
 
     public Vector2 controlInput;
 
@@ -44,39 +46,42 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        controlInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        if(animator)
+        {
+            stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+            controlInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
-        if (controlInput.magnitude < controlDeadZone)
-            controlInput = Vector2.zero;
-        else
-            controlInput = controlInput.normalized * ((controlInput.magnitude - controlDeadZone) / (1 - controlDeadZone));
+            if (controlInput.magnitude < controlDeadZone)
+                controlInput = Vector2.zero;
+            else
+                controlInput = controlInput.normalized * ((controlInput.magnitude - controlDeadZone) / (1 - controlDeadZone));
 
-        horizontal = controlInput.x * playerRot * Time.deltaTime;
-        vertical = controlInput.y * playerRun * Time.deltaTime;
+            horizontal = controlInput.x * playerRot * Time.deltaTime;
+            vertical = controlInput.y * playerRun * Time.deltaTime;
 
-        transform.Translate(0, 0, vertical);
-        transform.Rotate(0, horizontal, 0);
+            transform.Translate(0, 0, vertical);
+            transform.Rotate(0, horizontal, 0);
 
-        ControlToWorld(this.transform, playerCam.transform, ref direction, ref speed);
+            ControlToWorld(this.transform, playerCam.transform, ref direction, ref speed);
 
-        //speed = new Vector2(horizontal, vertical).magnitude; //for animations (.sqrMagnitude is faster)
+            //speed = new Vector2(horizontal, vertical).magnitude; //for animations (.sqrMagnitude is faster)
 
-        //animator.SetFloat("Speed", speed);
-        //animator.SetFloat("Direction", direction, dampDirectionTime, Time.deltaTime);
+            //animator.SetFloat("Speed", speed);
+            //animator.SetFloat("Direction", direction, dampDirectionTime, Time.deltaTime);
+        }
+    }
 
-        
-	}
+    private void FixedUpdate()
+    {
+        if (IsInLocomotion() && ((direction >= 0 && horizontal >= 0) || (direction < 0 && horizontal < 0)))
+        {
+            //lerps between 0 and 0, 1, 0 if stick is held right, or 0, -1, 0 is stick is held left
+            Vector3 rotationAmount = Vector3.Lerp(Vector3.zero, new Vector3(0f, rotDeg * (horizontal < 0.0f ? -1f : 1f), 0.0f), Mathf.Abs(horizontal));
+            Quaternion deltaRotation = Quaternion.Euler(rotationAmount * Time.deltaTime);
+            this.transform.rotation = this.transform.rotation * deltaRotation;
 
-    //private void FixedUpdate()
-    //{
-    //    if(IsInLocomotion() && ((direction >= 0 && horizontal >= 0) || (direction < 0 && horizontal < 0)))
-    //    {
-    //        Vector3 rotationAmount = Vector3.Lerp(Vector3.zero, new Vector3(0f, rotationDegreePerSecond * (horizontal < 0.0f ? -1f : 1f), 0.0f), Mathf.Abs)
-    //        Quaternion deltaRotation = Quaternion.Euler(rotationAmount * Time.deltaTime);
-    //        this.transform.rotation = this.transform.rotation * deltatRotation;
-
-    //    }
-    //}
+        }
+    }
 
     public void ControlToWorld(Transform playerTrans, Transform camera, ref float directionOut, ref float speedOut)
     {
@@ -108,8 +113,8 @@ public class PlayerController : MonoBehaviour {
         directionOut = angleplayerTransToMove * directionSpeed;
     }
 
-    //public bool IsInLocomotion()
-    //{
-    //    return stateInfo.nameHash == n_LocomotionID;
-    //}
+    public bool IsInLocomotion()
+    {
+        return stateInfo.fullPathHash == n_LocomotionID;
+    }
 }
