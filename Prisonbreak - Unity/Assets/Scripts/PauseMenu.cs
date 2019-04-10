6f1d1099048f80e7eeb9698048972a322792f503
Brendan Winston <bwinsto1@fsc.int>
2019-04-10 19:26:44 -0400

    Pause menu and controller menu support

---
+++ Prisonbreak - Unity/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject pauseMenu, ui;
+
+    private bool paused = false;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("pause"))
+        {
+            if (paused)
+            {
+                Resume();
+            }
+
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        ui.SetActive(true);
+        Time.timeScale = 1f;
+        paused = false;
+    }
+
+    public void Pause()
+    {
+        ui.SetActive(false);
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        paused = true;
+    }
+
+    public void ResumeGame()
+    {
+        Resume();
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
